7bd83e409bae717dc846143ff562e16deda4a372
nickcatalano5 <nickcatalano24@gmail.com>
2024-01-30 12:29:54 -0500

    deploying

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(express.static('../client/dist'));
+const PORT = mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/contactsDb"
+);
+app.use(express.static("../client/dist"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes.js')(app);
+require("./routes/htmlRoutes.js")(app);
 
 app.listen(PORT, function () {
   console.log(`Now listening on port: ${PORT}`);
